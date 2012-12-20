5fa92d9b5083266033116c59b4988e6052c639b0
Simeon F. Willbanks <sfw@simeonfosterwillbanks.com>
2012-12-20 12:10:08 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mygists::Application.config.secret_token = '70af082461ebc00eb19208aa77d928dbec2dbdd46e1f335d115d8a0df188c2134f778910df2ae0303fa8e5458bde63f02dda68f726a4569021028d52ad8eb239'
